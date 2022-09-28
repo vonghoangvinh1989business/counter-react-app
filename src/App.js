4598707d76dc419a22855ebabb4cf631183f2510
Vong Hoang Vinh <vonghoangvinh1989@gmail.com>
2022-09-28 18:49:25 +0700

    add code for counter app using react instead of vanilla javascript

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+  let color = count > 0 ? "green" : count < 0 ? "red" : "black";
+
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+  const resetCount = () => setCount(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="container">
+        <h1>Counter</h1>
+        <span style={{ color }} id="value">
+          {count}
+        </span>
+        <div className="btn-group">
+          <button onClick={decrement} className="btn btn-decrease">
+            DECREASE
+          </button>
+          <button onClick={resetCount} className="btn btn-reset">
+            RESET
+          </button>
+          <button onClick={increment} className="btn btn-increase">
+            INCREASE
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
